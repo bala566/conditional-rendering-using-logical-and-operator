4433dda689aad2c622b6eb9aab4977a323a11149
bala566 <baladurga566@gmail.com>
2022-09-23 11:58:27 +0000

    conditional rendering using element variables

--- src/App.js
+++ src/App.js
@@ -9,19 +9,20 @@ class App extends Component {
     isLoggedIn: true,
   }
 
-  renderAuthButton = () => {
-    const {isLoggedIn} = this.state
-    if (isLoggedIn === true) {
-      return <button>Logout</button>
-    }
-    return <button>Login</button>
-  }
-
   render() {
+    const {isLoggedIn} = this.state
+
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {this.renderAuthButton()}
+        {authButton}
       </div>
     )
   }
