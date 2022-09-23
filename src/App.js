3f8b480053f714dbe053e43733018ad93654e6c2
bala566 <baladurga566@gmail.com>
2022-09-23 12:06:27 +0000

    conditional rendering using ternary operator

--- src/App.js
+++ src/App.js
@@ -12,17 +12,10 @@ class App extends Component {
   render() {
     const {isLoggedIn} = this.state
 
-    let authButton
-    if (isLoggedIn === true) {
-      authButton = <button>Logout</button>
-    } else {
-      authButton = <button>Login</button>
-    }
-
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {authButton}
+        {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
       </div>
     )
   }
